b6a11fecd6c05fb8c8c44ddf38fe49ede55cdc42
Jordan McKible <jordan@mckible.com>
2013-12-07 12:43:49 -0800

    Rails 4.0.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Esophagus::Application.config.secret_key_base = '1946281a25b42c1bef4313ab84173eee247fcf76203a2ef44223d85626b6e12137725b431ba4589b22e5ecce2ea48bfcfa5d4e96ca05fbf47e161b6e70dfd98f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Esophagus::Application.config.session_store :cookie_store, key: '_esophagus_session'
